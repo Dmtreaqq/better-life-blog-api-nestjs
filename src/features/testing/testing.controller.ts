e5d35c968d89f7e11e8e49aa378042db87bb3dde
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-12-15 02:31:34 +0400

    feature: fix delete users after test

--- src/features/testing/testing.controller.ts
+++ src/features/testing/testing.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { BlogModelType } from '../bloggers-platform/domain/blog.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post, PostModelType } from '../bloggers-platform/domain/post.entity';
+import { UserModelType } from '../user-platform/domain/user.entity';
 
 @Controller('testing')
 export class TestingController {
@@ -10,6 +11,7 @@ export class TestingController {
     private BlogModel: BlogModelType,
     @InjectModel(Post.name)
     private PostModel: PostModelType,
+    private UserModel: UserModelType,
   ) {}
 
   @Delete('all-data')
@@ -17,5 +19,6 @@ export class TestingController {
   async deleteAll() {
     await this.BlogModel.deleteMany();
     await this.PostModel.deleteMany();
+    await this.UserModel.deleteMany();
   }
 }
