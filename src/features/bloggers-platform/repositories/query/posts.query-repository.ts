c812887c3880f9efcea11c1466b5205d5b197435
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-12-15 01:14:05 +0400

    fix tests

--- src/features/bloggers-platform/repositories/query/posts.query-repository.ts
+++ src/features/bloggers-platform/repositories/query/posts.query-repository.ts
@@ -5,20 +5,33 @@ import { PostViewDto } from '../../api/view-dto/post.view-dto';
 import { PostQueryGetParams } from '../../api/input-dto/get-posts-query.dto';
 import { BasePaginationViewDto } from '../../../../common/dto/base-pagination.view-dto';
 import { FilterQuery } from 'mongoose';
+import { BlogModelType } from '../../domain/blog.entity';
 
 @Injectable()
 export class PostsQueryRepository {
-  constructor(@InjectModel(Post.name) private PostModel: PostModelType) {}
+  constructor(
+    @InjectModel(Post.name) private PostModel: PostModelType,
+    @InjectModel('Blog') private BlogModel: BlogModelType,
+  ) {}
 
   async getAll(
     query: PostQueryGetParams,
     blogId: string = '',
   ): Promise<BasePaginationViewDto<PostViewDto[]>> {
-    const filter: FilterQuery<Post> = {};
-
-    if (blogId !== '') {
-      filter.blogId = blogId;
+    const blog = await this.BlogModel.findById(blogId);
+    if (!blog) {
+      throw new NotFoundException({
+        errorsMessages: [
+          {
+            message: 'Blog not found while get post',
+            field: 'blogId',
+          },
+        ],
+      });
     }
+
+    const filter: FilterQuery<Post> = {};
+    filter.blogId = blogId;
 
     const posts = await this.PostModel.find(filter)
       .sort({ [query.sortBy]: query.sortDirection })
