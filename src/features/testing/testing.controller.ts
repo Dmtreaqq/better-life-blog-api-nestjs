cc66a1597748605f42f3ec10cad750f90f3c9c64
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-12-15 02:42:15 +0400

    finish

--- src/features/testing/testing.controller.ts
+++ src/features/testing/testing.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Delete, HttpCode, HttpStatus } from '@nestjs/common';
 import { BlogModelType } from '../bloggers-platform/domain/blog.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Post, PostModelType } from '../bloggers-platform/domain/post.entity';
-import { UserModelType } from '../user-platform/domain/user.entity';
+import { User, UserModelType } from '../user-platform/domain/user.entity';
 
 @Controller('testing')
 export class TestingController {
@@ -11,6 +11,7 @@ export class TestingController {
     private BlogModel: BlogModelType,
     @InjectModel(Post.name)
     private PostModel: PostModelType,
+    @InjectModel(User.name)
     private UserModel: UserModelType,
   ) {}
 
