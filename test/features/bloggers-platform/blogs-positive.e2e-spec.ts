2628c4631c952c318558fe979c8d6ec6fedca3b6
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2024-12-16 22:23:56 +0400

    feature: added e2e tests for blogs

--- test/features/bloggers-platform/blogs-positive.e2e-spec.ts
+++ test/features/bloggers-platform/blogs-positive.e2e-spec.ts
@@ -9,6 +9,7 @@ import { API_PREFIX } from '../../../src/settings/global-prefix.setup';
 import { appSetup } from '../../../src/settings/app.setup';
 import { BlogsTestManager } from '../../helpers/blogs-test-manager';
 import { UpdateBlogInput } from '../../../src/features/bloggers-platform/api/input-dto/update-blog.dto';
+import { CreatePostInputDto } from '../../../src/features/bloggers-platform/api/input-dto/create-post-input.dto';
 
 describe('Blogs Positive (e2e)', () => {
   let app: INestApplication;
@@ -140,5 +141,106 @@ describe('Blogs Positive (e2e)', () => {
       .expect(HttpStatus.OK);
 
     expect(getResponse.body).toEqual({ ...blog, name: newBody.name });
+  });
+
+  it('should GET blogs using sorting successfully', async () => {
+    const blogs = await blogsTestManager.createBlogs(5);
+
+    const response1 = await request(app.getHttpServer())
+      .get(
+        `${API_PREFIX}${API_PATH.BLOGS}/?sortBy=name&sortDirection=asc&searchNameTerm=tor`,
+      )
+      .expect(HttpStatus.OK);
+
+    expect(response1.body.items[0].name).toEqual(blogs[0].name);
+
+    const response2 = await request(app.getHttpServer())
+      .get(
+        `${API_PREFIX}${API_PATH.BLOGS}/?sortBy=name&sortDirection=desc&searchNameTerm=tor`,
+      )
+      .expect(HttpStatus.OK);
+
+    expect(response2.body.items[0].name).toEqual(blogs[blogs.length - 1].name);
+  });
+
+  it('should GET blogs using pagination successfully', async () => {
+    const blogs = await blogsTestManager.createBlogs(5);
+
+    const getResponse = await request(app.getHttpServer())
+      .get(
+        `${API_PREFIX}${API_PATH.BLOGS}/?pageSize=2&pageNumber=2&searchNameTerm=${blogs[0].name.slice(0, 3)}`,
+      )
+      .expect(HttpStatus.OK);
+
+    expect(getResponse.body).toEqual({
+      items: expect.any(Array),
+      totalCount: 5,
+      pagesCount: 3,
+      pageSize: 2,
+      page: 2,
+    });
+    expect(getResponse.body.items).toHaveLength(2);
+  });
+
+  it('should POST post for a certain blog', async () => {
+    const blog = await blogsTestManager.createBlog({
+      name: 'Blog Name',
+      description: 'Desc',
+      websiteUrl: 'https://google.com',
+    });
+
+    const response = await request(app.getHttpServer())
+      .post(`${API_PREFIX}${API_PATH.BLOGS}/${blog.id}/posts`)
+      .send({
+        title: 'Post',
+        shortDescription: 'desc',
+        blogId: blog.id,
+        content: 'post content',
+      } as CreatePostInputDto)
+      // .set('authorization', authHeader)
+      .expect(HttpStatus.CREATED);
+
+    expect(response.body).toEqual({
+      title: 'Post',
+      shortDescription: 'desc',
+      blogId: blog.id,
+      content: 'post content',
+      id: expect.any(String),
+      createdAt: expect.any(String),
+      blogName: blog.name,
+      extendedLikesInfo: expect.any(Object),
+    });
+
+    const getResponse = await request(app.getHttpServer())
+      .get(`${API_PREFIX}${API_PATH.POSTS}/${response.body.id}`)
+      .expect(HttpStatus.OK);
+
+    expect(getResponse.body).toEqual({
+      title: 'Post',
+      shortDescription: 'desc',
+      blogId: blog.id,
+      content: 'post content',
+      id: expect.any(String),
+      createdAt: expect.any(String),
+      blogName: blog.name,
+      extendedLikesInfo: expect.any(Object),
+    });
+  });
+
+  it('should DELETE blog successfully', async () => {
+    const blog = await blogsTestManager.createBlog({
+      name: 'name',
+      websiteUrl: 'https://google.com',
+      description: 'some desc',
+    });
+
+    await request(app.getHttpServer())
+      .delete(`${API_PREFIX}${API_PATH.BLOGS}/${blog.id}`)
+      // .set('authorization', authHeader)
+      .expect(HttpStatus.NO_CONTENT);
+
+    await request(app.getHttpServer())
+      .get(`${API_PREFIX}${API_PATH.BLOGS}/${blog.id}`)
+      .expect(HttpStatus.NOT_FOUND);
   });
 });
--- test/helpers/blogs-test-manager.ts
+++ test/helpers/blogs-test-manager.ts
@@ -4,6 +4,8 @@ import * as request from 'supertest';
 import { CreateBlogInput } from '../../src/features/bloggers-platform/api/input-dto/create-blog.dto';
 import { API_PREFIX } from '../../src/settings/global-prefix.setup';
 import { BlogViewDto } from '../../src/features/bloggers-platform/api/view-dto/blog.view-dto';
+import { UserViewDto } from '../../src/features/user-platform/api/view-dto/users.view-dto';
+import { delay } from './delay';
 
 export class BlogsTestManager {
   constructor(private app: INestApplication) {}
@@ -17,5 +19,21 @@ export class BlogsTestManager {
       .expect(statusCode);
 
     return response.body;
+  }
+
+  async createBlogs(count: number) {
+    const blogsPromises = [] as Promise<BlogViewDto>[];
+
+    for (let i = 0; i < count; ++i) {
+      await delay(50);
+      const response = this.createBlog({
+        name: `BlogNametor ` + i,
+        description: `test${i}description`,
+        websiteUrl: `https://google${i}.com`,
+      });
+      blogsPromises.push(response);
+    }
+
+    return Promise.all(blogsPromises);
   }
 }
